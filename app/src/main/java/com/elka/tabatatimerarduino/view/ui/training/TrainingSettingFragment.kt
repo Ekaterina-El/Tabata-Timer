2e74a7394a35ba8449b457e2a78186b2b102cba1
EkaterinaEl <hackerrussia@yandex.ru>
2023-05-23 19:38:20 +0500

    Added animation by press to back/up training`s property

--- app/src/main/java/com/elka/tabatatimerarduino/view/ui/training/TrainingSettingFragment.kt
+++ app/src/main/java/com/elka/tabatatimerarduino/view/ui/training/TrainingSettingFragment.kt
@@ -1,10 +1,14 @@
 package com.elka.tabatatimerarduino.view.ui.training
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
+import com.elka.tabatatimerarduino.R
 import com.elka.tabatatimerarduino.databinding.TrainingSettingFragmentBinding
 
 class TrainingSettingFragment : TrainingConnection() {
@@ -21,6 +25,7 @@ class TrainingSettingFragment : TrainingConnection() {
     return binding.root
   }
 
+  @SuppressLint("ClickableViewAccessibility")
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
@@ -28,9 +33,52 @@ class TrainingSettingFragment : TrainingConnection() {
         disconnect()
       }
     })
-/*
-    val args = TrainingFragmentArgs.fromBundle(requireArguments())
-    bluetoothWorker.connectWith(args.device, bluetoothConnectionListener)*/
+
+    binding.cyclesBack.setOnTouchListener(touchListener)
+    binding.cyclesUp.setOnTouchListener(touchListener)
+    binding.setsBack.setOnTouchListener(touchListener)
+    binding.setsUp.setOnTouchListener(touchListener)
+    binding.workBack.setOnTouchListener(touchListener)
+    binding.workUp.setOnTouchListener(touchListener)
+    binding.restBack.setOnTouchListener(touchListener)
+    binding.restUp.setOnTouchListener(touchListener)
+    binding.restBetweenSetsBack.setOnTouchListener(touchListener)
+    binding.restBetweenSetsUp.setOnTouchListener(touchListener)
+  }
+
+  private val touchListener = object: View.OnTouchListener {
+    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+      val tag = view?.tag.toString() ?: return false
+      val action = event?.action ?: return false
+
+      val tagsParts = tag.split(".")
+      val dir = tagsParts[0]
+      val type = tagsParts[1]
+
+      when (action) {
+        MotionEvent.ACTION_DOWN -> {
+          val res = when(dir) {
+            "back" -> R.drawable.triangle_left_activate
+            "up" -> R.drawable.triangle_right_activate
+            else -> null
+          }
+          res?.let { (view as ImageView).setImageResource(res) }
+        }
+
+        MotionEvent.ACTION_UP -> {
+          val res = when(dir) {
+            "back" -> R.drawable.triangle_left
+            "up" -> R.drawable.triangle_right
+            else -> null
+          }
+          res?.let { (view as ImageView).setImageResource(res) }
+        }
+
+        else -> return false
+      }
+      return false
+    }
+
   }
 
 }
